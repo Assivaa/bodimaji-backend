0e3b7de26dafff0e62be885ddb4eb44be5f6bc9f
asiva syhrm <assiva.syahrani9e@gmail.com>
2022-12-29 23:19:28 +0700

    update backend cart and connect to mongodb

---
+++ backend/data/products.js
@@ -0,0 +1,11 @@
+const products = [
+       {
+              "name": "Bodimaji1",
+              "imageUrl": "https://cdnb.lystit.com/photos/thehut/f2727275/adidas-Blue-Duramo-Slide-Sandals.jpeg",
+              "description": "sss",
+              "price": 499,
+              "countInStock": 15,
+       },
+];
+
+module.exports = products;
