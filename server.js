f2e051bfc49e71c5a275a2b1356d6b5a3c644ff7
reyvido <reyvidod@gmail.com>
2023-01-02 13:12:53 +0700

    install cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./models/index");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const { collection, article } = require("./router");
 
 mongoose.set("strictQuery", false);
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/", [collection, article]);
 
 const PORT = process.env.PORT || 3001;
