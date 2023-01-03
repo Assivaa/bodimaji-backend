a725666729c6f8ddb3ea86d4e1832ab23ff3418a
asiva syhrm <assiva.syahrani9e@gmail.com>
2023-01-03 20:13:43 +0700

    fix error

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const connectDB = require("./models/index");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { collection, article, users } = require("./router");
-const { notFound, errorHandler } = require("./Middleware/Errors");
 
 mongoose.set("strictQuery", false);
 
@@ -15,8 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
-app.use(notFound);
-app.use(errorHandler);
+
 app.use("/", [collection, article, users]);
 
 const PORT = process.env.PORT || 3001;
