d6e69ddef2ef51147135f36a435ccd6a890155d1
reyvido <reyvidod@gmail.com>
2023-01-03 16:55:55 +0700

    change get API for collection to match asyncHandler (will do for all API endpoints), add error handler middleware

--- router/collection.js
+++ router/collection.js
@@ -1,3 +1,4 @@
+const asyncHandler = require("express-async-handler");
 const express = require("express");
 const router = express.Router();
 const Collection = require("../models/Collection");
@@ -6,41 +7,45 @@ router.get("/", async (req, res) => {
   res.status(200).json("Bodimaji API");
 });
 
-router.get("/collection/", async (req, res) => {
-  try {
-    let collection;
-    collection = await Collection.find();
-    res.status(200).json(collection);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get(
+  "/collection/",
+  asyncHandler(async (req, res) => {
+    const collection = await Collection.find({});
+    res.json(collection);
+  })
+);
+
+router.get(
+  "/collection/latest",
+  asyncHandler(async (req, res) => {
+    const collection = await Collection.find().sort({ createdAt: -1 }).limit(3);
+    if (collection) {
+      res.json(collection);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
+    }
+  })
+);
+
+router.get(
+  "/collection/:id",
+  asyncHandler(async (req, res) => {
+    const collection = await Collection.findById(req.params.id);
+    if (collection) {
+      res.json(collection);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
+    }
+  })
+);
 
 router.post("/collection", async (req, res) => {
   const newCollection = new Collection(req.body);
   try {
     const savedCollection = await newCollection.save();
     res.status(200).json(savedCollection);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/collection/latest", async (req, res) => {
-  try {
-    let collection;
-    collection = await Collection.find().sort({ createdAt: -1 }).limit(3);
-    res.status(200).json(collection);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/collection/:id", async (req, res) => {
-  try {
-    let collection;
-    collection = await Collection.findById(req.params.id);
-    res.status(200).json(collection);
   } catch (err) {
     res.status(500).json(err);
   }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./models/index");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { collection, article, users } = require("./router");
+const { notFound, errorHandler } = require("./Middleware/Errors");
 
 mongoose.set("strictQuery", false);
 
@@ -14,6 +15,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 app.use("/", [collection, article, users]);
 
 const PORT = process.env.PORT || 3001;
